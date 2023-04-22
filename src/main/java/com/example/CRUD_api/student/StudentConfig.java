9534bc4f4c20812a4e29a6780436bbd3bcd28705
rob3rt96 <robert.popescu195@gmail.com>
2023-04-22 17:56:10 +0300

    Add StudentConfig as a way to save new students with Bean methods

---
+++ src/main/java/com/example/CRUD_api/student/StudentConfig.java
@@ -0,0 +1,37 @@
+package com.example.CRUD_api.student;
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static java.time.Month.AUGUST;
+import static java.time.Month.DECEMBER;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+        return args -> {
+            Student marian = new Student(
+                    "Marian",
+                    22,
+                    LocalDate.of(2000, AUGUST, 10),
+                    "marian.gheorghe@gmail.com"
+            );
+
+            Student alex = new Student(
+                    "Alex",
+                    27,
+                    LocalDate.of(1995, DECEMBER, 22),
+                    "alex.manolache@gmail.com"
+            );
+
+            studentRepository.saveAll(List.of(marian, alex));
+        };
+    }
+}
