6842f6607bf98b8ee73582c481dfb507b40696f5
rob3rt96 <robert.popescu195@gmail.com>
2023-04-22 21:44:29 +0300

    Add business logic for adding new Students in the database

--- src/main/java/com/example/CRUD_api/student/StudentRepository.java
+++ src/main/java/com/example/CRUD_api/student/StudentRepository.java
@@ -1,10 +1,15 @@
 package com.example.CRUD_api.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    @Query("SELECT s FROM Student s WHERE s.email = ?1")    // The more specific query in JPQL
+    Optional<Student> findStudentByEmail(String email);
 }
